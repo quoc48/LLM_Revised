8fd052e0862204864f2c1ae1b1c0eda2936dcec6
quoc48 <thanh.quoc731@gmail.com>
2025-03-22 16:38:23 +0700

    Input embeddings

--- ch02/ch02.py
+++ ch02/ch02.py
@@ -98,13 +98,105 @@ import tiktoken
 
 tokenizer = tiktoken.get_encoding("gpt2")
 
-text = (
-    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
-     "of someunknownPlace."
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+enc_text = tokenizer.encode(raw_text)
+#print(len(enc_text))
+enc_sample = enc_text[50:] # get the first 50 tokens for demonstrating a text passage 
+
+# Create basic input-target pair
+context_size = 4
+x = enc_sample[:context_size]
+y = enc_sample[1:context_size+1]
+
+for i in range(1, context_size+1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    #print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
+
+#________________
+# A dataset for batched inputs and tartgets
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        # Tokenize the entire text
+        token_ids = tokenizer.encode(txt, allowed_special={"<|endoftext|>"})
+
+        # Use a sliding window to chunk the book into overlapping sequences of max_length
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+def create_dataloader_v1(txt, batch_size=4, max_length=256, 
+                         stride=128, shuffle=True, drop_last=True,
+                         num_workers=0):
+
+    # Initialize the tokenizer
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    # Create dataset
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+
+    # Create dataloader
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
+
+# test
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=2, max_length=4, stride=4, shuffle=False
 )
 
-integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
-print(integers)
+# Encode the input tokens into 256 -dimensional vector representation
+vocab_size = 50027
+output_dim = 256
 
-strings = tokenizer.decode(integers)
-print(strings)
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+max_length = 4
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=8, max_length=max_length,
+    stride=max_length, shuffle=False
+)
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+print("Token IDs:\n", inputs)
+print("\nInputs shape:\n", inputs.shape)
+
+token_embeddings = token_embedding_layer(inputs)
+print(token_embeddings.shape)
+
+# uncomment & execute the following line to see how the embeddings look like
+# print(token_embeddings)
+
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(max_length))
+print(pos_embeddings.shape)
+
+# Create embedding input used in an LLM, simply add the token embedding and the positional embedding:
+input_embeddings = token_embeddings + pos_embeddings
+print(input_embeddings.shape)
+print(input_embeddings)
