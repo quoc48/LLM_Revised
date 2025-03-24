db96c3d33280c622a34a31d7345fb1d57f599166
quoc48 <thanh.quoc731@gmail.com>
2025-03-24 22:24:32 +0700

    masked attention weight

--- ch03/ch03.py
+++ ch03/ch03.py
@@ -117,4 +117,31 @@ class SelfAttention_v2(nn.Module):
 
 torch.manual_seed(789)
 sa_v2 = SelfAttention_v2(d_in, d_out)
-print(sa_v2(inputs))
+#print(sa_v2(inputs))
+
+# Applying a causal attention mask
+# Reuse the query and key weight matrices of the
+# SelfAttention_v2 object from the previous section for convenience
+queries = sa_v2.W_query(inputs)
+keys = sa_v2.W_key(inputs) 
+attn_scores = queries @ keys.T
+
+attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+print(attn_weights)
+
+context_length = attn_scores.shape[0]
+mask_simple = torch.tril(torch.ones(context_length, context_length))
+print(mask_simple)
+
+masked_simple = attn_weights*mask_simple
+print(masked_simple)
+
+row_sums = masked_simple.sum(dim=-1, keepdim=True)
+masked_simple_norm = masked_simple / row_sums
+print(masked_simple_norm)
+
+mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+masked = attn_scores.masked_fill(mask.bool(), -torch.inf)
+print(masked)
+attn_weights = torch.softmax(masked / keys.shape[-1]**0.5, dim=1)
+print(attn_weights)
