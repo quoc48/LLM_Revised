e6c6002f3f4a8e6c37d3de63f069ed3c7713e337
quoc48 <thanh.quoc731@gmail.com>
2025-03-27 05:59:38 +0700

    Layer Normalization

--- ch04/ch04.py
+++ ch04/ch04.py
@@ -59,7 +59,7 @@ class DummyLayerNorm(nn.Module):
         # This layer does nothing and just returns its input.
         return x
 
-
+# examples
 tokenizer = tiktoken.get_encoding("gpt2")
 
 batch = []
@@ -76,5 +76,51 @@ torch.manual_seed(123)
 model = DummyGPTModel(GPT_CONFIG_124M)
 
 logits = model(batch)
-print("Output shape:", logits.shape)
-print(logits)
+#print("Output shape:", logits.shape)
+#print(logits)
+
+#_______________
+torch.manual_seed(123)
+
+# create 2 training examples with 5 dimensions (features) each
+batch_example = torch.randn(2, 5) 
+
+layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
+out = layer(batch_example)
+#print(out)
+
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+#print("Mean:\n", mean)
+#print("Variance:\n", var)
+
+out_norm = (out - mean) / torch.sqrt(var)
+#print("Normalized layer outputs:\n", out_norm)
+
+mean = out_norm.mean(dim=-1, keepdim=True)
+var = out_norm.var(dim=-1, keepdim=True)
+torch.set_printoptions(sci_mode=False)
+#print("Mean:\n", mean)
+#print("Variance:\n", var)
+
+# LayerNorm
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+    
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+mean = out_ln.mean(dim=-1, keepdim=True)
+var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
+
+print("Mean:\n", mean)
+print("Variance:\n", var)
