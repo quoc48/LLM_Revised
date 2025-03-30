da7d0745a15ad60f5a4ff7f15e0090bfe23d821b
quoc48 <thanh.quoc731@gmail.com>
2025-03-30 09:48:28 +0700

    finalize GPT model

--- ch04/ch04.py
+++ ch04/ch04.py
@@ -205,10 +205,96 @@ torch.manual_seed(123)
 model_without_shortcut = ExampleDeepNeuralNetwork(
     layer_sizes, use_shortcut=False
 )
-print_gradients(model_without_shortcut, sample_input)
+#print_gradients(model_without_shortcut, sample_input)
 
 torch.manual_seed(123)
 model_with_shortcut = ExampleDeepNeuralNetwork(
     layer_sizes, use_shortcut=True
 )
-print_gradients(model_with_shortcut, sample_input)
+#print_gradients(model_with_shortcut, sample_input)
+
+#___________________
+# The transformer block component of GPT
+
+from previous_chapters import MultiHeadAttention
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"], 
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"])
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        # Shortcut connection for attention block
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)  # Shape [batch_size, num_tokens, emb_size]
+        x = self.drop_shortcut(x)
+        x = x + shortcut  # Add the original input back
+
+        # Shortcut connection for feed forward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut  # Add the original input back
+
+        return x
+
+torch.manual_seed(123)
+
+x = torch.rand(2, 4, 768)  # Shape: [batch_size, num_tokens, emb_dim]
+block = TransformerBlock(GPT_CONFIG_124M)
+output = block(x)
+
+print("Input shape:", x.shape)
+print("Output shape:", output.shape)
+
+#__________________________
+# The GPT modek architecture implementation
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+            super().__init__()
+            self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+            self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+            self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        
+            self.trf_blocks = nn.Sequential(
+             *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+        
+            self.final_norm = LayerNorm(cfg["emb_dim"])
+            self.out_head = nn.Linear(
+                cfg["emb_dim"], cfg["vocab_size"], bias=False
+            )
+
+    def forward(self, in_idx):
+            batch_size, seq_len = in_idx.shape
+            tok_embeds = self.tok_emb(in_idx)
+            pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+            x = tok_embeds + pos_embeds  # Shape [batch_size, num_tokens, emb_size]
+            x = self.drop_emb(x)
+            x = self.trf_blocks(x)
+            x = self.final_norm(x)
+            logits = self.out_head(x)
+            return logits
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+
+out = model(batch)
+print("Input batch:\n", batch)
+print("\nOutput shape:", out.shape)
+print(out)
+
+total_params = sum(p.numel() for p in model.parameters())
+print(f"Total number of parameters: {total_params:,}")
